4c28ad5f22f600777c1b714d44459020cb9a4f6e
Holly A. Wallace <holly@senmetric.com>
2019-09-27 12:22:29 -0700

    Add New Item functionality complete

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Find Mike", "Buy Eggs", "Impeach Trump"]
+    var itemArray = ["Find Mike", "Buy Eggs", "Impeach Trump"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,6 +47,45 @@ class TodoListViewController: UITableViewController {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
     }
+    
+    //MARK: - add new items
+    
+    // This method is what will happen when the add new item button is pressed
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textFieldText = UITextField()
+        
+        // create a popup
+        let alert = UIAlertController(title: "Add New ToDo Item", message: "", preferredStyle: .alert)
+        
+        // create an action button for the popup
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // this is what happens when the user clicks the Add Item button on our UIAlert
+            
+            if textFieldText.text == "" {
+                // don't store a blank
+            }
+            else {
+                self.itemArray.append(textFieldText.text!)
+                self.tableView.reloadData()
+            }
+        }
+        
+        alert.addTextField { (alertTextField) in
+            // the placeholder is the pale grey text in the field that disappears once the user
+            // starts typing
+            alertTextField.placeholder = "Create new item"
+            textFieldText = alertTextField
+        }
+        
+        // add the action button to the popup
+        alert.addAction(action)
+        
+        // show the popup
+        present(alert, animated: true, completion: nil)
+        
+    } // end of addButtonPressed method
+    
 
 } // end of TodoListViewController class
 
